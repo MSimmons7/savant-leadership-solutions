3463d6cb1bc46b66f63aabd5d0a222c4d12261de
Michael Simmons <msimmo19@lion.lmu.edu>
2019-05-14 13:06:17 -0600

    Added php backend
    Need to test and iterate

--- contact.php
+++ contact.php
@@ -1,346 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Savant Leadership Solutions</title>
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <link href="assets/css/gaia.css" rel="stylesheet"/>
-
-    <!--     Fonts and icons     -->
-    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link href="assets/css/fonts/pe-icon-7-stroke.css" rel="stylesheet">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-</head>
-
-<body>
-
-    <nav class="navbar navbar-default navbar-transparent navbar-fixed-top" color-on-scroll="200">
-        <!-- if you want to keep the navbar hidden you can add this class to the navbar "navbar-burger"-->
-        <div class="container">
-            <div class="navbar-header">
-                <button id="menu-toggle" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar bar1"></span>
-                    <span class="icon-bar bar2"></span>
-                    <span class="icon-bar bar3"></span>
-                </button>
-                <a href="#" class="navbar-brand">
-                    <!-- <img src="assets/img/logowhite.png" style="width:50px;height:40px;"> -->
-                    Improving Minds, Improving Performance
-                </a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav navbar-right navbar-uppercase">
-                    <li>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            Menu
-                        </a>
-                        <ul class="dropdown-menu dropdown-danger">
-                            <li>
-                                <a href="#"><i class="fa fa-facebook-square"></i> Facebook</a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-twitter"></i> Twitter</a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-instagram"></i> Instagram</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-    </nav>
-
-
-    <div class="section section-header"><a name="home"></a>
-        <div class="parallax filter">
-            <div class="image"
-                style="background-image: url('assets/img/vistageheader.jpg')">
-            </div>
-            <div class="container">
-                <div class="content">
-                    <div class="title-area">
-                        <br>
-                        <h2 class="title-modern">Vistage</h2>
-                        <h3>It all happens when the groups meets.</h3>
-                        <div class="separator line-separator">♦</div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-
-
-    <div class="section">
-        <div class="container">
-            <div class="row">
-                <div class="title-area">
-                    <h2>Why Vistage?</h2>
-                    <div class="separator separator-danger">✻</div>
-                    <p class="description">Executive coahcing alone won't deliver sustained results.</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="info-icon">
-                        <div class="icon text-danger">
-                            <i class="pe-7s-users"></i>
-                        </div>
-                        <h3>Groups</h3>
-                        <p class="description">Exclusive, confidential CEOs and key executive groups with non-competing businesses.</p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="info-icon">
-                        <div class="icon text-danger">
-                            <i class="pe-7s-comment"></i>
-                        </div>
-                        <h3>Coaching</h3>
-                        <p class="description">One-to-one executive coaching from a Vistage Chair, an accomplished business leader.</p>
-                    </div>
-                </div>
-                <!-- <div class="col-md-4">
-                    <div class="info-icon">
-                        <div class="icon text-danger">
-                            <i class="pe-7s-micro"></i>
-                        </div>
-                        <h3>Experts</h3>
-                        <p class="description">Thought leaders and subject-matter experts who challenge your thinking at speaker workshops and events.</p>
-                    </div>
-                </div> -->
-                <div class="col-md-4">
-                    <div class="info-icon">
-                        <div class="icon text-danger">
-                            <i class="pe-7s-world"></i>
-                        </div>
-                        <h3>Community</h3>
-                        <p class="description">A global community of C-Suite leaders offering reliable advice on business & leadership development.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="section section-our-team-freebie">
-        <div class="parallax filter filter-color-black">
-            <div class="image" style="background-image:url('assets/img/header-2.jpeg')">
-            </div>
-            <div class="container">
-                <div class="content">
-                    <div class="row">
-                        <div class="title-area">
-                            <h2>Who We Are</h2>
-                            <div class="separator separator-danger">✻</div>
-                            <p class="description">Savant Leadership Solutions was founded out of an ardent passion to help others create positive change in their careers, their businesses, and in their lives.</p>
-                        </div>
-                    </div>
-
-                    <div class="team">
-                        <div class="row">
-                            <div class="col-md-10 col-md-offset-1">
-                                <div class="row">
-                                        <div class="card card-member">
-                                            <div class="content">
-                                                <div class="avatar avatar-danger">
-                                                    <img alt="..." class="img-circle" src="assets/img/Cathy.jpg"/>
-                                                </div>
-                                                <div class="description">
-                                                    <h3 class="title">Catherine Wicklund</h3>
-                                                    <p class="small-text">CEC / CEO of Savant Leadership</p>
-                                                    <p class="description">“I was drawn to coaching because of my innate desire to serve others and to help leaders focus on continuous improvement. I believe the old adage: ‘if you are not part of the solution, you are part of problem.’  My coaching assists clients in determining what the problem and gaps really are, and what steps they can take to create and move forward with their own solutions.  My trainings leave the audience with real skills and knowledge they can apply immediately.”</p>
-                                                </div>
-                                            </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="section section-our-clients-freebie">
-        <div class="container">
-            <div class="title-area">
-                <h5 class="subtitle text-gray">Here are some</h5>
-                <h2>Clients Testimonials</h2>
-                <div class="separator separator-danger">∎</div>
-            </div>
-
-            <ul class="nav nav-text" role="tablist">
-                <li class="active">
-                    <a href="#testimonial1" role="tab" data-toggle="tab">
-                        <div class="image-clients">
-                            <img alt="..." class="img-circle" src="assets/img/profile.png"/>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#testimonial2" role="tab" data-toggle="tab">
-                        <div class="image-clients">
-                            <img alt="..." class="img-circle" src="assets/img/profile.png"/>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#testimonial3" role="tab" data-toggle="tab">
-                        <div class="image-clients">
-                            <img alt="..." class="img-circle" src="assets/img/profile.png"/>
-                        </div>
-                    </a>
-                </li>
-            </ul>
-
-
-            <div class="tab-content">
-                <div class="tab-pane active" id="testimonial1">
-                    <p class="description">
-                        "Catherine has natural leadership and development skills that make her an excellent coach.  While many people have to learn these skills, one of Catherine’s innate passions and abilities is to guide others toward creating and reaching their personal and professional goals.  She has a gentle, yet solid coaching style; she doesn’t tell someone where their passions are, but asks the right questions to guide them to discover it on their own.  Catherine has the professionalism and adaptability to work with and develop anyone from a janitor to a CEO.  She is a unique, rare personality that thrives in developing others full time.”  ~Amanda K.
-                    </p>
-                </div>
-                <div class="tab-pane" id="testimonial2">
-                    <p class="description">"Catherine possesses all of the coaching skills that make her a natural personal and professional coach:  great listener, ability to ask questions in a way that makes a person think about another perspective, approachable, friendly, open demeanor and open mind, and – above all – she is trustworthy.”  ~ Terri L.
-                    </p>
-                </div>
-                <div class="tab-pane" id="testimonial3">
-                    <p class="description">"Catherine is a natural coach – she has a positive attitude and interacts effectively with those around her.  She provides beneficial feedback to help move things forward, and she’s able to synthesize and help you express your own ideas in a way that makes sense.  She’s incredibly knowledgeable and is able to offer different viewpoints and perspectives.  She’s realistic, sincere, and works to identify the underlying or core problem.  She’s a great listener and communicator and truly pays attention to details.  She is someone you want in your court."  ~ Mike V.
-                    </p>
-                </div>
-
-            </div>
-
-        </div>
-    </div>
-
-
-    <div class="section section-small section-get-started">
-        <div class="parallax filter">
-            <div class="image"
-                style="background-image: url('assets/img/office-1.jpeg')">
-            </div>
-            <div class="container">
-                <div class="title-area">
-                    <h2 class="text-white">Want to get in touch right away?</h2>
-                    <div class="separator line-separator">♦</div>
-                    <p class="description"> We look forward to working with you! Drop us a line and we will get back to you as soon as we can.</p>
-                </div>
-
-                <div class="button-get-started">
-                    <a href="mailto:catherine@savantleadership.com" class="btn btn-danger btn-fill btn-lg">Contact Us</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <footer class="footer footer-big footer-color-black" data-color="black">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-2 col-sm-3">
-                    <div class="info">
-                        <h5 class="title">Company</h5>
-                        <nav>
-                            <ul>
-                                <li>
-                                    <a href="#home">Home</a></li>
-                                <li>
-                                    <a href="#">Find offers</a>
-                                </li>
-                                <li>
-                                    <a href="#">Discover Projects</a>
-                                </li>
-                                <li>
-                                    <a href="#">Our Portfolio</a>
-                                </li>
-                                <li>
-                                    <a href="#">About Us</a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-                <div class="col-md-3 col-md-offset-1 col-sm-3">
-                    <div class="info">
-                        <h5 class="title"> Help and Support</h5>
-                         <nav>
-                            <ul>
-                                <li>
-                                    <a href="#">Contact Us</a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-3">
-                    <div class="info">
-                        <h5 class="title">Latest News</h5>
-                        <nav>
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-twitter"></i> This is a test for what a blog will look like...
-                                        <hr class="hr-small">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-twitter"></i> This is another test for a blog...
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-                <div class="col-md-2 col-md-offset-1 col-sm-3">
-                    <div class="info">
-                        <h5 class="title">Follow us on</h5>
-                        <nav>
-                            <ul>
-                                <li>
-                                    <a href="https://www.linkedin.com/in/cwicklund/" class="btn btn-social btn-reddit btn-simple">
-                                        <i class="fa fa-linkedin"></i> LinkedIn
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-            <hr>
-            <div class="copyright">
-                 © <script> document.write(new Date().getFullYear()) </script> Savant Leadership Solution LLC
-            </div>
-        </div>
-    </footer>
-
-</body>
-
-<!--   core js files    -->
-<script src="assets/js/jquery.min.js" type="text/javascript"></script>
-<script src="assets/js/bootstrap.js" type="text/javascript"></script>
-
-<!--  js library for devices recognition -->
-<script type="text/javascript" src="assets/js/modernizr.js"></script>
-
-<!--  script for google maps   -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
-
-<!--   file where we handle all the script from the Gaia - Bootstrap Template   -->
-<script type="text/javascript" src="assets/js/gaia.js"></script>
-
-</html>
+<?php
+ 
+if($_POST) {
+    $name = "";
+    $email = "";
+    $company_name = "";
+    $message = "";
+     
+    if(isset($_POST['name'])) {
+        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
+    }
+     
+    if(isset($_POST['email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+    }
+     
+    if(isset($_POST['company_name'])) {
+        $email_title = filter_var($_POST['email_title'], FILTER_SANITIZE_STRING);
+    }
+     
+    if(isset($_POST['message'])) {
+        $visitor_message = htmlspecialchars($_POST['visitor_message']);
+    }
+    $recipient = 'msimmo19@lion.lmu.edu';
+     
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $email . "\r\n";
+     
+    if(mail($recipient, $company_name, $message, $headers)) {
+        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+     
+} else {
+    echo '<p>Something went wrong</p>';
+}
+ 
+?>
