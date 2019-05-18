7345c3aae6637522276ea6342e491d41f0601db1
Michael Simmons <msimmo19@lion.lmu.edu>
2019-05-18 11:14:16 -0600

    PHP test

--- contact.php
+++ contact.php
@@ -1,41 +1,12 @@
-<?php
- 
-if($_POST) {
-    $name = "";
-    $email = "";
-    $company_name = "";
-    $message = "";
-     
-    if(isset($_POST['name'])) {
-        $name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
-    }
-     
-    if(isset($_POST['email'])) {
-        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
-        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-    }
-     
-    if(isset($_POST['company_name'])) {
-        $company_name = filter_var($_POST['company_name'], FILTER_SANITIZE_STRING);
-    }
-     
-    if(isset($_POST['message'])) {
-        $message = htmlspecialchars($_POST['message']);
-    }
-    $recipient = 'msimmo19@lion.lmu.edu';
-     
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . $email . "\r\n";
-     
-    if(mail($recipient, $company_name, $message, $headers)) {
-        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
-    } else {
-        echo '<p>We are sorry but the email did not go through.</p>';
-    }
-     
-} else {
-    echo '<p>Something went wrong</p>';
-}
- 
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$company_name = $_POST['company_number'];
+$number = $_POST['number'];
+$formcontent="From: $name \n Message: $message \n Company Name: $company_name \n Phone Number: $number";
+$recipient = "msimmo19@lion.lmu.edu";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
